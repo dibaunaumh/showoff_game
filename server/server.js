4da5dbaece422f9214b7d23c82a66ad60cc58ac5
dibau <dibaunaumh@gmail.com>
2014-02-23 12:54:56 +0200

    minor refactoring

--- server/server.js
+++ server/server.js
@@ -11,11 +11,9 @@ Meteor.publish("showoffs", function() {
 Accounts.onCreateUser(function(options, user) {
 	
     if (user.services.google){
-        var accessToken = user.services.google.accessToken,
-            result,
-            profile;
-
-        result = Meteor.http.get("https://www.googleapis.com/oauth2/v3/userinfo?alt=json", {
+        var accessToken = user.services.google.accessToken;
+		
+        var result = Meteor.http.get("https://www.googleapis.com/oauth2/v3/userinfo?alt=json", {
             params: {
                 access_token: accessToken
             }
@@ -24,7 +22,7 @@ Accounts.onCreateUser(function(options, user) {
         if (result.error)
             throw result.error;
 
-        profile = _.pick(result.data,
+        user.profile = _.pick(result.data,
             "login",
             "name",
             "avatar_url",
@@ -34,12 +32,9 @@ Accounts.onCreateUser(function(options, user) {
             "blog",
             "location",
             "email");
-
-        user.profile = profile;
-
     }
 	
-	var data = {
+	createPlayer({
 		name: user.profile.name,
 		picture: user.profile.picture,
 		location:  getRandomLocation(BOARD_WIDTH, BOARD_HEIGHT),
@@ -47,7 +42,7 @@ Accounts.onCreateUser(function(options, user) {
 		glyph: user.profile.name[0],
 		score: 0,
 		user_id: user._id
-	};
-	createPlayer(data);
+	});
+	
 	return user;
 });
